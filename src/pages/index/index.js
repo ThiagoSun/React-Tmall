7c62c6c14b453a4e2ebe7f2084af341f18b8d285
Sun Mengge <qiang261369985@gmail.com>
2018-05-10 23:50:15 +0800

    config layout and routers

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.less';
-import App from './App';
+import createStore from './store/createStore';
 import registerServiceWorker from '../../registerServiceWorker';
+import './index.less';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(window.__INITIAL_STATE__);
+
+const MOUNT_NODE = document.getElementById('root');
+
+let render = () => {
+  const App = require('./container/App').default;
+  const routes = require('./routes/index').default(store);
+
+  ReactDOM.render(
+    <App store={store} routes={routes} />,
+    MOUNT_NODE
+  )
+};
+
+render();
 registerServiceWorker();
